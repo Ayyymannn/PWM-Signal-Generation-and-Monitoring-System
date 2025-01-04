bc32e7e0a7e955ed0394eced4b6cb435b73f52db
Ayman Al Rubaey <123348733+Ayyymannn@users.noreply.github.com>
2025-01-04 02:17:21 -0800

    Update Project.c

--- Project.c
+++ Project.c
@@ -9,27 +9,6 @@
 #include "cmsis/cmsis_device.h"
 
 // ----------------------------------------------------------------------------
-//
-// STM32F0 led blink sample (trace via $(trace)).
-//
-// In debug configurations, demonstrate how to print a greeting message
-// on the trace device. In release configurations the message is
-// simply discarded.
-//
-// To demonstrate POSIX retargetting, reroute the STDOUT and STDERR to the
-// trace device and display messages on both of them.
-//
-// Then demonstrates how to blink a led with 1Hz, using a
-// continuous loop and SysTick delays.
-//
-// On DEBUG, the uptime in seconds is also displayed on the trace device.
-//
-// Trace support is enabled by adding the TRACE macro definition.
-// By default the trace messages are forwarded to the $(trace) output,
-// but can be rerouted to any device or completely suppressed, by
-// changing the definitions required in system/src/diag/trace_impl.c
-// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
-//
 // The external clock frequency is specified as a preprocessor definition
 // passed to the compiler via a command line option (see the 'C/C++ General' ->
 // 'Paths and Symbols' -> the 'Symbols' tab, if you want to change it).
@@ -41,8 +20,7 @@
 // so please adjust the PLL settings in system/src/cmsis/system_stm32f0xx.c
 //
 // ----- main() ---------------------------------------------------------------
-// Sample pragmas to cope with warnings. Please note the related line at
-// the end of this function, used to pop the compiler diagnostics status.
+
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
